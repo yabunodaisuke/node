d82fdd8b03fa49dab65582599d06c0bd72fdd995
yabunodaisuke <69379587+yabunodaisuke@users.noreply.github.com>
2021-04-05 19:43:51 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,57 +1,40 @@
-const functions = require("firebase-functions");
-const express = require("express");
-const requestPromise = require("request-promise-native");
-const cors = require("cors");
-
-const app = express();
-
-// app.use(cors()); 
-
-// APIにリクエストを送る関数を定義
-const getDataFromApi = async (keyword) => {
-    // cloud functionsから実行する場合には地域の設定が必要になるため，`country=JP`を追加している
-    const requestUrl =
-      "https://www.googleapis.com/books/v1/volumes?country=JP&q=intitle:";
-    const result = await requestPromise(`${requestUrl}${keyword}`);
-    return result;
-  };
-
-app.get("/hello", (req, res) => {
-  res.send("Hello Express!");
-});
-
-// ↓↓↓ エンドポイントを追加 ↓↓↓
-app.get("/user/:userId", (req, res) => {
-  const users = [
-    { id: 1, name: "ジョナサン" },
-    { id: 2, name: "ジョセフ" },
-    { id: 3, name: "承太郎" },
-    { id: 4, name: "仗助" },
-    { id: 5, name: "ジョルノ" },
-  ];
-  // req.params.userIdでURLの後ろにつけた値をとれる．
-  const targetUser = users.find(
-    (user) => user.id === Number(req.params.userId)
-  );
-  res.send(targetUser);
-});
-
-// エンドポイント追加
-app.get("/gbooks/:keyword", async (req, res) => {
-    // APIリクエストの関数を実行
-    const response = await getDataFromApi(req.params.keyword);
-    res.send(response);
-  });
-
-// 以降変更なし
-const api = functions.https.onRequest(app);
-module.exports = { api };
-
-
-//local
-
-// http://localhost:5000/node1-c253b/us-central1/helloWorld
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const request = require('request'); 
+var API_KEY = '5fa1e5948ead05d2b41ef481ceb634bf'; 
+  
+const forecast = function (latitude, longitude) { 
+  
+var url = `http://api.openweathermap.org/data/2.5/weather?`
+            +`lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
+  
+    request({ url: url, json: true }, function (error, response) { 
+        if (error) { 
+            console.log('Unable to connect to Forecast API'); 
+        } 
+          else { 
+  
+            console.log('It is currently '
+                + response.body.main.temp
+                + ' degrees out.'
+            ); 
+  
+            console.log('The high today is '
+                + response.body.main.temp_max 
+                + ' with a low of '
+                + response.body.main.temp_min
+            ); 
+  
+            console.log('Humidity today is '
+                + response.body.main.humidity
+            ); 
+        } 
+    }) 
+} 
+  //東京
+// var latitude = 35.676192; // Indore latitude 
+// var longitude = 139.650311; // Indore longitude 
+  
+//  大阪
+var latitude = 34.693725; // Indore latitude 
+var longitude = 135.502254; // Indore longitude 
+// Function call 
+forecast(latitude, longitude); 
